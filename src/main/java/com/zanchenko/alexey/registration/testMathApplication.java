d6f9568c188b88572c90ec996acf7ee75919c53d
williambrownqqq <alexey.zanchenko@gmail.com>
2023-06-19 17:09:19 +0300

    fix 1

--- src/main/java/com/zanchenko/alexey/registration/testMathApplication.java
+++ src/main/java/com/zanchenko/alexey/registration/testMathApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegistrationApplication {
+public class testMathApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RegistrationApplication.class, args);
+		SpringApplication.run(testMathApplication.class, args);
 	}
 
 }
